a3859b4b3254095db787bd7a2ba6a648c554dff7
rm-miyatake <89385120+rm-miyatake@users.noreply.github.com>
2021-08-27 12:25:43 +0900

    adminルーティング修正、RouteServiceProvider設定

--- routes/admin.php
+++ routes/admin.php
@@ -1,14 +1,25 @@
 <?php
 
-use App\Http\Controllers\Auth\Admin\AuthenticatedSessionController;
-use App\Http\Controllers\Auth\Admin\ConfirmablePasswordController;
-use App\Http\Controllers\Auth\Admin\EmailVerificationNotificationController;
-use App\Http\Controllers\Auth\Admin\EmailVerificationPromptController;
-use App\Http\Controllers\Auth\Admin\NewPasswordController;
-use App\Http\Controllers\Auth\Admin\PasswordResetLinkController;
-use App\Http\Controllers\Auth\Admin\RegisteredUserController;
-use App\Http\Controllers\Auth\Admin\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Admin\Auth\ConfirmablePasswordController;
+use App\Http\Controllers\Admin\Auth\EmailVerificationNotificationController;
+use App\Http\Controllers\Admin\Auth\EmailVerificationPromptController;
+use App\Http\Controllers\Admin\Auth\NewPasswordController;
+use App\Http\Controllers\Admin\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Admin\Auth\RegisteredUserController;
+use App\Http\Controllers\Admin\Auth\VerifyEmailController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
 Route::get('/dashboard', function () {
     return view('admin.dashboard');
